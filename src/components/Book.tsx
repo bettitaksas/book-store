eea1d7acd2facb9b4eb040c84cc555b25a366f5f
bettitaksas <109687064+bettitaksas@users.noreply.github.com>
2023-08-12 11:42:25 +0200

    format price with formatCurrency.tsx

--- src/components/Book.tsx
+++ src/components/Book.tsx
@@ -1,4 +1,5 @@
 import { Card } from "react-bootstrap"
+import { formatCurrency } from "../utilities/formatCurrency"
 
 type BookProps = {
     id: number,
@@ -7,7 +8,7 @@ type BookProps = {
     imgUrl: string
 }
 
-export function Book({id, name, price, imgUrl}: BookProps) {
+export function Book({ id, name, price, imgUrl }: BookProps) {
     return (
         <Card>
             <Card.Img
@@ -15,8 +16,17 @@ export function Book({id, name, price, imgUrl}: BookProps) {
                 src={imgUrl}
                 width="200px"
                 height="300px"
-                style={{objectFit: "cover"}}
+                style={{ objectFit: "cover" }}
             />
+            <Card.Body className="d-flex flex-column">
+                <Card.Title className="d-flex
+                    justify-content-between
+                    align-items-baseline
+                    mb-5">
+                    <span className="fs-2">{name}</span>
+                    <span className="ms-2 text-muted">{formatCurrency(price)}</span>
+                </Card.Title>
+            </Card.Body>
         </Card>
     )
 }
