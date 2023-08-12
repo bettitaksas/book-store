1bcf61c87c361dd7204d3a23c48fe22a597d8bf2
bettitaksas <109687064+bettitaksas@users.noreply.github.com>
2023-08-12 17:03:30 +0200

    display books with img, quantity and price in cart

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,18 +1,26 @@
-import { Offcanvas } from "react-bootstrap";
+import { Offcanvas, Stack } from "react-bootstrap";
 import { useCart } from "../context/CartContext";
+import { CartItem } from "./CartItem";
 
 type CartProps = {
     isOpen: boolean
 }
 
 export function Cart({isOpen}: CartProps) {
-    const {closeCart} = useCart()
+    const {closeCart, booksInCart} = useCart()
 
   return (
     <Offcanvas show={isOpen} onHide={closeCart} placement="end">
       <Offcanvas.Header closeButton>
         <Offcanvas.Title>Cart</Offcanvas.Title>
       </Offcanvas.Header>
+      <Offcanvas.Body>
+        <Stack gap={4}>
+            {booksInCart.map(book => (
+                <CartItem key={book.id} {...book} />
+            ))}
+        </Stack>
+      </Offcanvas.Body>
     </Offcanvas>
   );
 }
---
+++ src/components/CartItem.tsx
@@ -0,0 +1,39 @@
+import { Button, Stack } from "react-bootstrap"
+import { useCart } from "../context/CartContext"
+import storeBooks from "../data/books.json"
+import { formatCurrency } from "../utilities/formatCurrency"
+
+type CartItemProps = {
+    id: number
+    quantity: number
+}
+
+export function CartItem({id, quantity}: CartItemProps){
+    const {removeFromCart} = useCart()
+    const book = storeBooks.find(book => book.id === id)
+    if (book === null) return null
+
+    return (
+        <Stack direction="horizontal" gap={3} className="d-flex
+            align-items-center">
+            <img src={book?.imgUrl} style={{width: "120px", height: "180px", objectFit: "cover"}} />
+            <div className="me-auto">
+                <div>
+                    {book?.name}
+                    {quantity > 1 && 
+                        <span
+                        className="text-muted" style={{fontSize: ".8rem"}}>
+                            x{quantity}
+                        </span>}
+                </div>
+                <div className="text-muted" style={{fontSize: ".9rem"}}>
+                    {book && book.price !== undefined ? formatCurrency(book.price) : "Price not available"}
+                </div>
+                <div>
+                    {book && book.price !== undefined ? formatCurrency(book.price * quantity) : "Price not available"}
+                </div>
+            </div>
+            <Button variant="outline-danger" size="sm" onClick={() => book && book.price !== undefined ? removeFromCart(book.id) : "Price not available"}>&times;</Button>
+        </Stack>
+    )
+}
